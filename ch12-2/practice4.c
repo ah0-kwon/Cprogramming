76358b6b104cfd489ab9a28dcd5f2628e0f50563
권아영 <kwonoaoaoaoa@gmail.com>
2026-09-08 23:37:09 +0900

    Update practice4.c

--- ch12-2/practice4.c
+++ ch12-2/practice4.c
@@ -3,3 +3,20 @@
 // 날  짜  :  2026년 9월8일
 // 작성자  :  2600015 권아영
 // **********************************************
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 100, b = 200;
+    int sum;
+
+    int* pa = &a;
+    int* pb = &b;
+    int* psum = &sum;
+
+    *psum = *pa + *pb;
+
+    printf("두정수의 합 : %d\n", *psum);
+    return 0;
+}
