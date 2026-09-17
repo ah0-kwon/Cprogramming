2f7068dad242e72bd4ea6c2b279bd7699e1aa122
권아영 <kwonoaoaoaoa@gmail.com>
2026-09-17 23:54:50 +0900

    Create practice4.c

---
+++ ch13-2/practice4.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#pragma warning(disable:6031)  
+#include <stdio.h>
+
+int main(void)
+{
+    char a1[100];
+    char a2[100];
+    printf("문자열을 입력하시오: ");
+    scanf("%s", a1);
+    printf("문자열을 입력하시오: ");
+    scanf("%s", a2);
+
+    if (a1[0] < a2[0])
+    {
+        printf("사전에서 앞에 나오는 문자열: %s\n", a1);
+        return 0;
+    }
+    else if (a1[0] > a2[0])
+    {
+        printf("사전에서 앞에 나오는 문자열: %s\n", a2);
+        return 0;
+    }     
+    return 0;
+}
