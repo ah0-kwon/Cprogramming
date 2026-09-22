c83c04dbb234a2cba13b00b404781cd4331186b8
권아영 <kwonoaoaoaoa@gmail.com>
2026-09-22 23:40:03 +0900

    Add practice3.c with integer addition function

---
+++ ch14-1/practice3.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#pragma warning(disable:6031) 
+
+#include <stdio.h>
+
+int add2(int value);   // 함수 선언
+
+int main(void)
+{
+    int number;
+
+    printf("정수를 입력하세요: ");
+    scanf("%d", &number);
+
+    number = add2(number);   // 함수 호출
+
+    printf("2만큼 증가한 값: %d\n", number);
+    return 0;
+}
+
+int add2(int value)   // 함수 정의
+{
+    value += 2;
+    return value;
+}
